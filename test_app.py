498d2874f6391824cc2d17d2c43785ecf5e68a28
vedantdesai765 <vedant.ud@somaiya.edu>
2026-08-25 14:59:35 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,1 +1,13 @@
+from app import app
 
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello from AWS DevOps Lab!" in response.data
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert b"Application is healthy" in response.data
